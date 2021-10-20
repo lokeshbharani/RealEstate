987a38e32b9a161bd52574d2f67b1521977d745a
Lokesh Keyan <lokeshbharanikumarjk@gmail.com>
2021-10-19 20:31:13 -0400

    Add IProperty for attributes

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { IProperty } from "../IProperty";
 
 @Component({
   selector: 'app-property-card',
@@ -7,5 +8,6 @@ import { Component, Input } from "@angular/core";
 })
 
 export class PropertyCardComponent{
-  @Input() property : any
+  @Input()
+  property!: IProperty;
 }
